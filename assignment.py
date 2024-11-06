e24a1dd43c90fbc2b7f4be90c5402119113489ac
juliaburketerreau <julia.burke-terreau@stu.ocsb.ca>
2024-11-06 15:11:14 +0000

    changed spacing

--- assignment.py
+++ assignment.py
@@ -4,15 +4,14 @@ while ( redo ):
     pizza = input( "Large or Extra Large?" )
     print ("You selected", pizza)
     if pizza != "Large" and pizza != "Extra Large":
-    print("We do not have that size")
+        print("We do not have that size")
     else:
-    redo = False
+        redo = False
 if pizza == "Large":
     print("That costs $6")
     large = 6
     print("How many toppings, 1, 2, 3 or 4?")
     toppings = input()
-
     if toppings == "1":
         top_cost = 1
     elif toppings == "2":
@@ -21,25 +20,26 @@ if pizza == "Large":
         top_cost = 2.50
     elif toppings == "4":
         top_cost = 3.35
-        subtotal = float(large + top_cost)
-        import math
-        subtotal = math.floor(subtotal * 100) / 100
-        print("Your subtotal is")
-        print("$", subtotal)
-        tax = float(0.13 * subtotal)
-        tax = math.floor(tax * 100) / 100
-        print("Your tax is")
-        print("$", tax)
-        fintot = float(tax + subtotal)
-        fintot = math.floor(fintot * 100) / 100
-        print("Your final cost is")
-        print("$", fintot)
+    
+    subtotal = float(large + top_cost)
+    import math
+    subtotal = math.floor(subtotal * 100) / 100
+    print("Your subtotal is")
+    print("$", subtotal)
+    tax = float(0.13 * subtotal)
+    tax = math.floor(tax * 100) / 100
+    print("Your tax is")
+    print("$", tax)
+    fintot = float(tax + subtotal)
+    fintot = math.floor(fintot * 100) / 100
+    print("Your final cost is")
+    print("$", fintot)
 
     
 if pizza == "Extra Large":
     print("That costs $10")
     extralarge = 10
-    print("How many toppings. 1, 2, 3or 4?")
+    print("How many toppings. 1, 2, 3 or 4?")
     toppings = input()
     if toppings == "1":
         top_cost = 1
